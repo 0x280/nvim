48db0bdecbd252d7ad2852b1a782f8cf95d6012f
0x280 <86424319+0x280@users.noreply.github.com>
2024-09-21 16:27:09 +0200

    lsp setup

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,11 @@
 return {
-	'nvim-telescope/telescope.nvim', 
+	'nvim-telescope/telescope.nvim',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	keys = {
 		{ "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
-		{ "<C-p>", "<cmd>Telescope git_files<cr>", desc = "Telescope git files" },
-		{ "<leader>pg", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep" },
-		{ "<leader>pb", "<cmd>Telescope buffers<cr>", desc = "Telescope buffers" },
-		{ "<leader>ph", "<cmd>Telescope help_tags<cr>", desc = "Telescope help tags" },
+		{ "<C-p>",      "<cmd>Telescope git_files<cr>",  desc = "Telescope git files" },
+		{ "<leader>pg", "<cmd>Telescope live_grep<cr>",  desc = "Telescope live grep" },
+		{ "<leader>pb", "<cmd>Telescope buffers<cr>",    desc = "Telescope buffers" },
+		{ "<leader>ph", "<cmd>Telescope help_tags<cr>",  desc = "Telescope help tags" },
 	},
 }
